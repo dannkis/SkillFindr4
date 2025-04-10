f303b859a3ac8ca3b37625e941974d617dc46093
Daniil Zhelyazkov <62340093+dannkis@users.noreply.github.com>
2025-04-10 22:48:30 +0100

    Update SkillFindr.js

--- skillfindr/src/components/SkillFindr/SkillFindr.js
+++ skillfindr/src/components/SkillFindr/SkillFindr.js
@@ -59,7 +59,7 @@ const ChatbotPopup = () => {
     <FlexGrid condensed>
       <Column>
         {!isOpen && (
-          //button for skillfindr chatbot
+          //button for the skillfindr chatbot
           <IconButton
             renderIcon={ChatLaunch}
             label=""
